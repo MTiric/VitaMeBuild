89e2154193c7c74c6f76cb5449b12284ea412b1b
John Doe <johndoe@example>
2023-12-04 16:52:18 +0100

    Updates, updates

--- app/src/main/java/com/example/vitamebuild/MainActivity.kt
+++ app/src/main/java/com/example/vitamebuild/MainActivity.kt
@@ -5,12 +5,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.vitamebuild.classes.Food
 import com.example.vitamebuild.ui.theme.VitaMeBuildTheme
@@ -45,6 +50,27 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditTextField(
+    @StringRes label: Int,
+    //@DrawableRes leadingIcon: Int, //reserved for the leading icon, design phase
+    keyboardOptions: KeyboardOptions,
+    value: String, //current value to display
+    onValueChanged: (String) -> Unit, //callback lambda, this is triggered when the value changes, makes the state be updated elsewhere
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = value,
+        singleLine = true,
+        //leadingIcon = { Icon(painter = painterResource(id = leadingIcon), null) },
+        modifier = modifier,
+        onValueChange = onValueChanged,
+        label = { Text(stringResource(label)) },
+        keyboardOptions = keyboardOptions
     )
 }
 
